7b56cb21ca2ba98ea545d06ffe92a1a652cde95b
adibhaque <ash33@njit.edu>
2020-08-02 14:22:03 -0400

    Task: app.py updates

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'mlbData'
+app.config['MYSQL_DATABASE_DB'] = 'mlbplayerData'
 mysql.init_app(app)
 
 
@@ -20,53 +20,55 @@ mysql.init_app(app)
 def index():
     user = {'username': 'Adib Haques Project'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM mlb')
+    cursor.execute('SELECT * FROM mlbplayerData')
     result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, mlb=result)
+    return render_template('index.html', title='Home', user=user, mlbplayers=result)
 
 
 @app.route('/view/<int:mlb_id>', methods=['GET'])
 def record_view(mlb_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM mlb WHERE id=%s', mlb_id)
+    cursor.execute('SELECT * FROM mlbplayerData WHERE id=%s', mlb_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', mlb=result[0])
+    return render_template('view.html', title='View Form', mlbplayer=result[0])
 
 
 @app.route('/edit/<int:mlb_id>', methods=['GET'])
 def form_edit_get(mlb_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM mlb WHERE id=%s', mlb_id)
+    cursor.execute('SELECT * FROM mlbplayerData WHERE id=%s', mlb_id)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', mlb=result[0])
+    return render_template('edit.html', title='Edit Form', mlbplayer=result[0])
 
 
-@app.route('/edit/<int:grade_id>', methods=['POST'])
+@app.route('/edit/<int:mlb_id>', methods=['POST'])
 def form_update_post(mlb_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Name'), request.form.get('Team'), request.form.get('Position'),
-                 request.form.get('Height_inches'), request.form.get('Weight_lbs'),
-                 request.form.get('Age'), mlb_id)
-    sql_update_query = """UPDATE mlb t SET t.Name = %s, t.Team = %s, t.Position = %s, t.Height_inches = 
+    inputData = (
+        request.form.get('Name'), request.form.get('Team'), request.form.get('Position'),
+        request.form.get('Height_inches'), request.form.get('Weight_lbs'),
+        request.form.get('Age'), mlb_id)
+    sql_update_query = """UPDATE mlbplayerData t SET t.Name = %s, t.Team = %s, t.Position = %s, t.Height_inches = 
     %s, t.Weight_lbs = %s, t.Age = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/mlb/new', methods=['GET'])
+@app.route('/mlbplayers/new', methods=['GET'])
 def form_insert_get():
     return render_template('new.html', title='New MLB Player Form')
 
 
-@app.route('/mlb/new', methods=['POST'])
+@app.route('/mlbplayers/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Name'), request.form.get('Team'), request.form.get('Position'),
-                 request.form.get('Height_inches'), request.form.get('Weight_lbs'),
-                 request.form.get('Age'),)
-    sql_insert_query = """INSERT INTO mlb (Name, Team, Position, Height_inches, Weight_lbs, Age) VALUES (%s, %s,
-    %s, %s,%s, %s) """
+    inputData = (
+        request.form.get('Name'), request.form.get('Team'), request.form.get('Position'),
+        request.form.get('Height_inches'), request.form.get('Weight_lbs'),
+        request.form.get('Age'))
+    sql_insert_query = """INSERT INTO mlbplayerData (Name, Team, Position, Height_inches, Weight_lbs, Age) VALUES (%s, %s,
+    %s, %s, %s, %s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -75,48 +77,48 @@ def form_insert_post():
 @app.route('/delete/<int:mlb_id>', methods=['POST'])
 def form_delete_post(mlb_id):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM mlb WHERE id = %s """
+    sql_delete_query = """DELETE FROM mlbplayerData WHERE id = %s """
     cursor.execute(sql_delete_query, mlb_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/mlb', methods=['GET'])
-def api_browse() -> str:
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM mlb')
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    resp = Response(json_result, status=200, mimetype='application/json')
-    return resp
+#@app.route('/api/v1/mlb', methods=['GET'])
+#def api_browse() -> str:
+#    cursor = mysql.get_db().cursor()
+#   cursor.execute('SELECT * FROM mlb')
+#  result = cursor.fetchall()
+# json_result = json.dumps(result);
+#    resp = Response(json_result, status=200, mimetype='application/json')
+#    return resp
 
 
-@app.route('/api/v1/grades/<int:mlb_id>', methods=['GET'])
-def api_retrieve(mlb_id) -> str:
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM mlb WHERE id=%s', mlb_id)
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    resp = Response(json_result, status=200, mimetype='application/json')
-    return resp
+#@app.route('/api/v1/grades/<int:mlb_id>', methods=['GET'])
+#def api_retrieve(mlb_id) -> str:
+#    cursor = mysql.get_db().cursor()
+#    cursor.execute('SELECT * FROM mlb WHERE id=%s', mlb_id)
+#    result = cursor.fetchall()
+#    json_result = json.dumps(result);
+#    resp = Response(json_result, status=200, mimetype='application/json')
+#    return resp
 
 
-@app.route('/api/v1/mlb/', methods=['POST'])
-def api_add() -> str:
-    resp = Response(status=201, mimetype='application/json')
-    return resp
+#@app.route('/api/v1/mlb/', methods=['POST'])
+#def api_add() -> str:
+#    resp = Response(status=201, mimetype='application/json')
+#    return resp
 
 
-@app.route('/api/v1/mlb/<int:mlb_id>', methods=['PUT'])
-def api_edit(mlb_id) -> str:
-    resp = Response(status=201, mimetype='application/json')
-    return resp
+#@app.route('/api/v1/mlb/<int:mlb_id>', methods=['PUT'])
+#def api_edit(mlb_id) -> str:
+#    resp = Response(status=201, mimetype='application/json')
+#    return resp
 
 
-@app.route('/api/grades/<int:mlb_id>', methods=['DELETE'])
-def api_delete(mlb_id) -> str:
-    resp = Response(status=210, mimetype='application/json')
-    return resp
+#@app.route('/api/grades/<int:mlb_id>', methods=['DELETE'])
+#def api_delete(mlb_id) -> str:
+#    resp = Response(status=210, mimetype='application/json')
+#    return resp
 
 
 if __name__ == '__main__':
